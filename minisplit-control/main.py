9d2b7889c4eacc5034026a9b838050a7993293c4
Peter <peterHoburg@users.noreply.github.com>
2023-10-15 10:41:20 -0400

    Started adding version features.

---
+++ minisplit-control/main.py
@@ -0,0 +1,22 @@
+"""
+Version 0.1:
+* Set Heat and cool mode
+* Set temperature
+* schedule temp changes
+
+Version 0.2:
+* Read from external thermometer
+* Set temperature based on external thermometer
+* schedule temp changes based on external thermometer
+
+Version 0.3:
+"""
+
+def set_temperature(temp):
+    """Set the temperature of the minisplit"""
+    pass
+
+def set_mode(mode):
+    """Set the mode of the minisplit"""
+    pass
+
